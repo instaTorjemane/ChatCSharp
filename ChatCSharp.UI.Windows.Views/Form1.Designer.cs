1cb2f1d0183dee329d2a50804b3e44c369c80022
Yossra <torjemaneyosra@gmail.com>
2012-04-13 16:26:57 +0200

    Ajout d'un form avec début menu

--- ChatCSharp.UI.Windows.Views/Form1.Designer.cs
+++ ChatCSharp.UI.Windows.Views/Form1.Designer.cs
@@ -28,12 +28,113 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.seConnecterÀToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.actionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.envoyerUnFihierToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.igniorerContactToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.obtenirDeLaideToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aProposDeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.réaccepterContactToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.seConnecterÀToolStripMenuItem,
+            this.actionToolStripMenuItem,
+            this.helpsToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(528, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // seConnecterÀToolStripMenuItem
+            // 
+            this.seConnecterÀToolStripMenuItem.Name = "seConnecterÀToolStripMenuItem";
+            this.seConnecterÀToolStripMenuItem.Size = new System.Drawing.Size(120, 24);
+            this.seConnecterÀToolStripMenuItem.Text = "Se Connecter à";
+            // 
+            // actionToolStripMenuItem
+            // 
+            this.actionToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.envoyerUnFihierToolStripMenuItem,
+            this.igniorerContactToolStripMenuItem,
+            this.réaccepterContactToolStripMenuItem});
+            this.actionToolStripMenuItem.Name = "actionToolStripMenuItem";
+            this.actionToolStripMenuItem.Size = new System.Drawing.Size(64, 24);
+            this.actionToolStripMenuItem.Text = "Action";
+            // 
+            // envoyerUnFihierToolStripMenuItem
+            // 
+            this.envoyerUnFihierToolStripMenuItem.Name = "envoyerUnFihierToolStripMenuItem";
+            this.envoyerUnFihierToolStripMenuItem.Size = new System.Drawing.Size(207, 24);
+            this.envoyerUnFihierToolStripMenuItem.Text = "Envoyer Un Fihier";
+            // 
+            // igniorerContactToolStripMenuItem
+            // 
+            this.igniorerContactToolStripMenuItem.Name = "igniorerContactToolStripMenuItem";
+            this.igniorerContactToolStripMenuItem.Size = new System.Drawing.Size(207, 24);
+            this.igniorerContactToolStripMenuItem.Text = "Igniorer Contact";
+            // 
+            // helpsToolStripMenuItem
+            // 
+            this.helpsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.obtenirDeLaideToolStripMenuItem,
+            this.aProposDeToolStripMenuItem});
+            this.helpsToolStripMenuItem.Name = "helpsToolStripMenuItem";
+            this.helpsToolStripMenuItem.Size = new System.Drawing.Size(56, 24);
+            this.helpsToolStripMenuItem.Text = "helps";
+            this.helpsToolStripMenuItem.Click += new System.EventHandler(this.helpsToolStripMenuItem_Click);
+            // 
+            // obtenirDeLaideToolStripMenuItem
+            // 
+            this.obtenirDeLaideToolStripMenuItem.Name = "obtenirDeLaideToolStripMenuItem";
+            this.obtenirDeLaideToolStripMenuItem.Size = new System.Drawing.Size(194, 24);
+            this.obtenirDeLaideToolStripMenuItem.Text = "Obtenir De L\'aide";
+            // 
+            // aProposDeToolStripMenuItem
+            // 
+            this.aProposDeToolStripMenuItem.Name = "aProposDeToolStripMenuItem";
+            this.aProposDeToolStripMenuItem.Size = new System.Drawing.Size(194, 24);
+            this.aProposDeToolStripMenuItem.Text = "A Propos De";
+            // 
+            // réaccepterContactToolStripMenuItem
+            // 
+            this.réaccepterContactToolStripMenuItem.Name = "réaccepterContactToolStripMenuItem";
+            this.réaccepterContactToolStripMenuItem.Size = new System.Drawing.Size(207, 24);
+            this.réaccepterContactToolStripMenuItem.Text = "Réaccepter Contact";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(528, 337);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem seConnecterÀToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem actionToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem envoyerUnFihierToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem igniorerContactToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem obtenirDeLaideToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aProposDeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem réaccepterContactToolStripMenuItem;
     }
 }
 
